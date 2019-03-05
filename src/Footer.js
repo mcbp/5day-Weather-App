9e32ad209e5004cec02f0f3d53cfab77d5de5d10
Michael Priest <michael-priest@hotmail.com>
2019-03-05 00:14:14 +0000

    Updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 class Footer extends Component {
+
 	render() {
+		
+		var spanStyle = {
+			padding: "0 20px",
+		}
+		
 		return (
 			<footer>
 				<a href="https://github.com/mcbp"><i class="fab fa-github"></i> mcbp</a>
+				<span style={spanStyle}></span>
+				<a href="https://openweathermap.org"><i class="fas fa-cloud"></i> made with OpenWeatherMap API</a>
 			</footer>
 		)
+		
 	}
 }
 
