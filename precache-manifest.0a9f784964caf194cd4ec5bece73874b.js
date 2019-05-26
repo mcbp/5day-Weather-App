00e35bcda899c0bbdabf489d41a5011e6b9a8f9e
Michael Priest <michael-priest@hotmail.com>
2019-05-26 03:59:55 +0100

    Updates

---
+++ precache-manifest.0a9f784964caf194cd4ec5bece73874b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73e4f8f15050ef73f345",
+    "url": "/5day-Weather-App/static/js/runtime~main.73e4f8f1.js"
+  },
+  {
+    "revision": "60ecfb9ead7f1c1780fb",
+    "url": "/5day-Weather-App/static/js/main.60ecfb9e.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/5day-Weather-App/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "60ecfb9ead7f1c1780fb",
+    "url": "/5day-Weather-App/static/css/main.20ac6f5f.chunk.css"
+  },
+  {
+    "revision": "cf18fa6e918851d97fa390f1c5b11ec0",
+    "url": "/5day-Weather-App/index.html"
+  }
+];
